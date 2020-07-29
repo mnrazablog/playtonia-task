ffbf048c44b723d2c580f4a492edb1e9e3af0c59
mnraza <mnraza94@gmail.com>
2020-07-29 17:47:16 +0530

    final commit

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container">
+<div class="row">
+<div class="col-sm-3" v-for="todo of todos"  
+  v-bind:key="todo.id">
+  <div class="card text-white bg-dark mb-3" 
+  style="max-width: 18rem;" >
+  <div class="card-body">
+    <h5 class="card-title">{{todo.title}}</h5>
+    <p class="text-success " v-if="todo.completed==true">Completed <i class="fa fa-check-circle fa-2x" aria-hidden="true"></i></p>
+    <p class="text-danger " v-else>Not Completed <i i class="fa fa-close fa-2x" aria-hidden="true"></i> </p>
+  </div>  
+  </div>
+</div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props:['todos']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
